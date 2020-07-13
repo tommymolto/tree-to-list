6ffe8f80f70ce20496b800674315375f0a548e7f
paulo.marinho <paulo.marinho@compasso.com.br>
2020-07-13 15:06:50 -0300

    Atualizando parametros

--- treetolist.js
+++ treetolist.js
@@ -1,13 +1,21 @@
-function treetolist( array, parent )  {
-    const filhos = array.filter( x => x.ID_TOGGLEPAI === parent.ID_TOGGLE);
-    console.log('filhos de '+parent.ID_TOGGLE +' = ',filhos.length);
+const defaultOptions = {
+    key_id: 'id' ,
+    key_parent: 'parent' ,
+    key_child: 'children'
+};
+function treetolist( array, parent , options = {})  {
+    options = Object.assign({}, defaultOptions, options);
+    const { key_id, key_parent, key_child } = options;
+
+    const filhos = array.filter( x => x[key_parent] === parent[key_id]);
+    // console.log('filhos de '+parent[key_id] +' = ',filhos.length);
     if(filhos && filhos.length > 0){
-        parent['children'] = [];
+        parent[key_child] = [];
         array = array.filter( ( el ) => !filhos.includes( el ) );
         filhos.forEach(x => {
-            const p = treetolist(array, x);
-            //console.log('filhos de '+ x.ID_TOGGLE,p);
-            parent['children'].push(p);
+            const p = treetolist(array, x, options);
+            // console.log('filhos de '+ x.ID_TOGGLE,p);
+            parent[key_child].push(p);
         });
         return parent;
     }else{
